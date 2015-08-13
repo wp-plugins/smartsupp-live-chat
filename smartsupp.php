c35a88f5fca510f447670fd1ccdf2abd76b88a51
marekgach <marekgach@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-13 13:53:05 +0000

    fix in render
    git-svn-id: https://plugins.svn.wordpress.org/smartsupp-live-chat/trunk@1219999 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- smartsupp.php
+++ smartsupp.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Smartsupp Live Chat
  * Plugin URI:        http://www.smartsupp.com
  * Description:       Adds Smartsupp Live Chat code to your Wordpress.
- * Version:           0.2.0
+ * Version:           0.2.1
  * Author:            Tom Wawrosz
  * Author URI:        http://www.smartsupp.com
  * Text Domain:       smartsupp
